79c367a3aad5c6e5bee4243986d70101ab36b2cd
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-09-01 19:54:07 -0300

    add: connection with sql database

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,31 @@
 package main;
 
+import database.Conexao;
+//import database.Conexao2;
+import database.DadosConexao;
+
 public class Main {
+	public static Conexao con;
+	public static DadosConexao dadosCon = new DadosConexao();
+	public static final String BANCO = "SistemaCadastroDB";
+	public static final String SCHEMA = "sistema";
+	public static final String LOCAL = "localhost";
+	public static final String USUARIO = "postgres";
+	public static final String SENHA = "<teste123>";
+	public static final String PORTA = "5432";
+	public static final String BD = "PostgreSql";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		dadosCon.setBanco(BANCO);
+		dadosCon.setLocal(LOCAL);
+		dadosCon.setUser(USUARIO);
+		dadosCon.setSenha(SENHA);
+		dadosCon.setPorta(PORTA);
+		dadosCon.setBd(BD);
+
+		con = new Conexao(dadosCon);
+		con.conect();
 	}
 
 }
